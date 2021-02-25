9f0cc8c91d74335ea5c76ada139b62dae9d2d206
rajdeep-dabral <mail.rajdeepdabral@gmail.com>
2021-02-25 13:21:03 +0530

    Project added!

---
+++ src/main/java/PrintHelloWorld.java
@@ -0,0 +1,8 @@
+
+public class PrintHelloWorld {
+    public static void main(String [] args){
+        System.out.print("Rajdeep says @");
+        System.out.println("*****Hello World****");
+        System.out.println("*****Bye****");
+    }
+}
---
+++ src/test/java/PrintHelloWorldTest.java
@@ -0,0 +1,5 @@
+
+
+public class PrintHelloWorldTest {
+    //testing class
+}
