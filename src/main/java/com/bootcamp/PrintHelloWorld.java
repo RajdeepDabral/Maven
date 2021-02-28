530a3e3d7bcce2c48da64a358117ae58bc4a8bc1
rajdeep-dabral <mail.rajdeepdabral@gmail.com>
2021-02-28 18:06:50 +0530

    Completed

--- src/main/java/com/bootcamp/PrintHelloWorld.java
+++ src/main/java/com/bootcamp/PrintHelloWorld.java
@@ -1,5 +1,7 @@
+package com.bootcamp;
 
 public class PrintHelloWorld {
+
     public static void main(String [] args){
         System.out.print("Rajdeep says @");
         System.out.println("*****Hello World****");
